a8743be03dadfeea916d0b28f5fb8be285f59d48
Manuel Marcos <manuelignaciomarcos@hotmail.com>
2022-06-26 19:05:12 -0300

    Creating repository

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//FOR
+
+/*
+let nombre, apellido, dni;
+
+for(let i = 0; i<5; i++){
+    nombre = prompt("Ingrese su nombre:");
+    apellido = prompt("Ingrese su apellido:");
+    dni = parseInt(prompt("Ingrese su dni:"));
+    alert(`Buen dia \n-${nombre}\n-${apellido}\n-DNI:${dni}`);
+} */
+
+//WHILE
+
+/* let usuarioAceptado = "Manuel";
+let usuarioIngresado = prompt("Por favor ingrese su nombre de usuario:");
+
+while (usuarioIngresado != usuarioAceptado){
+    alert("El usuario ingresado no esta habilitado");
+    usuarioIngresado = prompt("Por favor ingrese su nombre de usuario:");
+}
+alert("Usuario correcto, ingresando..") */
+
+//DO WHILE
+do{
+    numero = prompt("Ingrese un numero");
+}while(parseInt(numero));
+
+
+
+
+
+
