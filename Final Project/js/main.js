11804b23f46c26d249d876732d2c74c40532cacb
Manuel Marcos <manuelignaciomarcos@hotmail.com>
2022-07-13 00:42:32 -0300

    Adding some changes but there are a lot of errors to check

--- Final Project/js/main.js
+++ Final Project/js/main.js
@@ -114,26 +114,28 @@ class Licencia{
 }
 
 class Inventario{
-    cosntructor(){
+    constructor(){
         this.equipos = [];
         this.licencias = [];
     }
 
     agregarEquipo(equipo){
-        equipos.push(equipo);
+        this.equipos.push(equipo);
     }
 
     agregarLicencia(licencia){
-        licencias.push(licencia);
+        this.licencias.push(licencia);
     }
 
-    buscarEquipo(tipo){
-        const equiposDisponibles = equipos.filter((el) => el.tipo.includes(tipo));
-        if (equiposDisponibles != undefined){
-            return equiposDisponibles;
+    buscarEquipo(tipo, user){
+        alert("HOLA");
+        const hayDisponibles = (this.equipos).some((el) => el.tipo == tipo);
+        if (hayDisponibles == false){
+            alert(`No hay ${tipo} disponibles en el invetario`)
         }
         else{
-            return undefined;
+            const equiposDisponibles = (this.equipos).filter((el) => el.tipo == tipo);
+            elegirItem(equiposDisponibles);
         }
     }
 }
@@ -182,19 +184,25 @@ function menu(user, inventario){
             while(selectedOrder != "1" && selectedOrder != "2"){
                 selectedOrder = prompt("Opcion incorrecta \n Selecciona una opcion: \n 1.Hardware \n 2.Software").toLowerCase();
             }
-            if (selectedOrder == "hardware"){
-                selectedHardware = prompt("Por favor ingrese el hardware solicitado (ESC para finalizar):").toLowerCase();
-                while (selectedHardware != "esc" && selectedHardware != "pc" && selectedHardware != "mouse" && selectedHardware != "teclado" && selectedHardware != "monitor"){ //Validacion del tipo de hardware
-                    equiposDisponibles = inventario.buscarEquipo(selectedHardware);
-                    if (equiposDisponibles != undefined){
-                        
-                    }
-                    alert("Producto agregado correctamente");
-                    selectedHardware = prompt("Seleccione otro elemento de hardware o ESC para finalizar:");
+            if (selectedOrder == "1"){//Hardware
+                selectedHardware = prompt("Por favor seleccione el tipo de hardware: \n 1. PC \n 2. Monitor \n 3. Teclado \n 4. Mouse")
+                switch(selectedHardware){
+                    case 1: //PC
+                        selectedHardware = "pc"
+                        let pcsDisponibles = inventario.buscarEquipo(selectedHardware,user); //Busco las pcs en el inventario
+                        alert(pcsDisponibles);
+                    case 2: //Monitor
+                        selectedHardware = "monitor"
+                        let monitoresDisponibles = inventario.buscarEquipo(selectedHardware); //Busco monitores en el inventario
+                    case 3: //Teclado
+                        selectedHardware = "teclado"
+                        let tecladosDisponibles = inventario.buscarEquipo(selectedHardware); //Busco teclados disponibles en el inventario
+                    case 4: //Mouse
+                        selectedHardware = "mouse"
+                        let mousesDisponibles = inventario.buscarEquipo(selectedHardware); //Busco mouses disponibles en el inventario
                 }
-                user.mostrarEquipos();
             }
-            else{
+            else{//Software
                 selectedSoftware = prompt("Nombre del software del cual requiere la licencia:").toUpperCase();
                 while (selectedSoftware != "esc"){ //Validacion del tipo de hardware
                     let licencia = new Licencia(selectedSoftware);
@@ -212,35 +220,63 @@ function menu(user, inventario){
 
 }
 
+//Cargamos inventario
+const inventario = new Inventario();
+//PCs
+let pc1 = new Equipo("pc", "latitude 7420" , "dell" , "iWgRr9Za", 09/2025, 290.000)
+inventario.agregarEquipo(pc1)
+let pc2 = new Equipo("pc", "latitude 7420" , "dell" , "3wUXMH2b", 09/2025, 290.000)
+inventario.agregarEquipo(pc2)
+let pc3 = new Equipo("pc", "probook 440" , "hp" , "9BTuHNed", 09/2023, 180.000)
+inventario.agregarEquipo(pc3)
+let pc4 = new Equipo("pc", "probook 440" , "hp" , "YR6oru6o", 09/2023, 180.000)
+inventario.agregarEquipo(pc4)
+//Mouses
+let mouse1 = new Equipo("mouse", "m280", "logitech", "7kPfkFkR", 12/2022, 1.500)
+inventario.agregarEquipo(mouse1)
+let mouse2 = new Equipo("mouse", "m280", "logitech", "nmvLs9na", 12/2022, 1.500)
+inventario.agregarEquipo(mouse2)
+let mouse3 = new Equipo("mouse", "dx-110", "genius", "GwsZand8", 8/2022, 500 )
+inventario.agregarEquipo(mouse3)
+let mouse4 = new Equipo("mouse", "dx-110", "genius", "QHDQ6Z3Y", 8/2022, 500 )
+inventario.agregarEquipo(mouse4)
+//Teclados
+let teclado1 = new Equipo("teclado", "k120", "logitech", "A8xYzgGq", 12/2022, 1.300 )
+inventario.agregarEquipo(teclado1)
+let teclado2 = new Equipo("teclado", "k120", "logitech", "5Cfq4CdD", 12/2022, 1.300 )
+inventario.agregarEquipo(teclado2)
+let teclado3 = new Equipo("teclado", "kb-118", "genius", "ygz6ScnT", 11/2022, 1.500 )
+inventario.agregarEquipo(teclado3)
+let teclado4 = new Equipo("teclado", "kb-118", "genius", "pF3p2tUq", 11/2022, 1.500 )
+inventario.agregarEquipo(teclado4)
+//Monitor
+let monitor1 = new Equipo("monitor", "22mn430h", "lg", "kMcPi4Dc", 4/2025, 48.000 )
+inventario.agregarEquipo(monitor1)
+let monitor2 = new Equipo("monitor", "22mn430h", "lg", "wMDVZsXT", 4/2025, 48.000 )
+inventario.agregarEquipo(monitor2)
+let monitor3 = new Equipo("monitor", "f390", "samsung", "W247RU5H", 12/2024, 45.000 )
+inventario.agregarEquipo(monitor3)
+let monitor4 = new Equipo("monitor", "f390", "samsung", "r4VTDEXo", 12/2024, 45.000 )
+inventario.agregarEquipo(monitor4)
+
+
+
 function main(){
     user = login(); //Retorna objeto usuario con sus propiedades (nombre, apellido, interno, correo, celular, sector)
-    inventario = cargarInventario();
-    menu(user); 
+    /* inventario = cargarInventario(); */
+    menu(user, inventario); 
+}
+
+function elegirItem(listaEquipos, user){
+    alert("Elija el equipo deseado, segun el numero de aparicion")
+    for (equipo of listaEquipos){
+        equipo.mostrarDatos();
+    }
+    selectedHardware = propt("Ingrese el numero:");
+    user.asignarEquipo(listaEquipos[selectedHardware]);
 }
 
 
-
-//Cargamos inventario
-//PCs
-let pc1 = new Equipo("pc", "latitude 7420" , "dell" , "iWgRr9Za", 09/2025, 290.000)
-let pc2 = new Equipo("pc", "latitude 7420" , "dell" , "3wUXMH2b", 09/2025, 290.000)
-let pc3 = new Equipo("pc", "probook 440" , "hp" , "9BTuHNed", 09/2023, 180.000)
-let pc4 = new Equipo("pc", "probook 440" , "hp" , "YR6oru6o", 09/2023, 180.000)
-//Mouses
-let mouse1 = new Equipo("mouse", "m280", "logitech", "7kPfkFkR", 12/2022, 1.500)
-let mouse2 = new Equipo("mouse", "m280", "logitech", "nmvLs9na", 12/2022, 1.500)
-let mouse3 = new Equipo("mouse", "dx-110", "genius", "GwsZand8", 8/2022, 500 )
-let mouse4 = new Equipo("mouse", "dx-110", "genius", "QHDQ6Z3Y", 8/2022, 500 )
-//Teclados
-let teclado1 = new Equipo("teclado", "k120", "logitech", "A8xYzgGq", 12/2022, 1.300 )
-let teclado2 = new Equipo("teclado", "k120", "logitech", "5Cfq4CdD", 12/2022, 1.300 )
-let teclado3 = new Equipo("teclado", "kb-118", "genius", "ygz6ScnT", 11/2022, 1.500 )
-let teclado4 = new Equipo("teclado", "kb-118", "genius", "pF3p2tUq", 11/2022, 1.500 )
-//Monitor
-let monitor1 = new Equipo("monitor", "22mn430h", "lg", "kMcPi4Dc", 4/2025, 48.000 )
-let monitor2 = new Equipo("monitor", "22mn430h", "lg", "wMDVZsXT", 4/2025, 48.000 )
-let monitor3 = new Equipo("monitor", "f390", "samsung", "W247RU5H", 12/2024, 45.000 )
-let monitor4 = new Equipo("monitor", "f390", "samsung", "r4VTDEXo", 12/2024, 45.000 )
 
 
 
